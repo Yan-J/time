a180f2137ca6f86280a88768ee4c034e1f4f8a00
Yan-J <yan_j1213@126.com>
2016-10-12 10:03:18 +0800

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def time():
+	import time
+	ticks = time.time()	
+	#print(ticks)
+	ticks_int = int(ticks)
+	return str(ticks_int)
+	
+
+if __name__ == "__main__":
+    app.run()
